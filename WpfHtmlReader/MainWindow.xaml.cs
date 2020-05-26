31c4238f426c67e9b8c1f82b9e93b2a0fe09aa26
GIn <sodj94@yandex.ru>
2020-05-26 15:53:29 +0300

    Add UI and functionality

--- WpfHtmlReader/MainWindow.xaml.cs
+++ WpfHtmlReader/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,81 @@ namespace WpfHtmlReader
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        string filename;
+        List<string> listOfUrls = new List<string>();
+
+        private void buttonOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog openDialog = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension 
+            openDialog.DefaultExt = ".txt";
+            openDialog.Filter = "TXT Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = openDialog.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                filename = openDialog.FileName;
+                textBlockFilePath.Text = filename;
+            }
+        }
+
+        private async void buttonStartSearching_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBlockFilePath.Text))
+            {
+                MessageBox.Show("Не выбран файл для работы.");
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(textBoxSearching.Text))
+            {
+                MessageBox.Show("Нет введённого текста для поиска.");
+                return;
+            }
+
+            string textFromSearchingTextBox = textBoxSearching.Text;
+
+            List<string> listOfStringsFromFile = new List<string>();
+            listOfStringsFromFile = File.ReadLines(filename).ToList();
+            listOfUrls = UrlValidation.ValidationCheck(listOfStringsFromFile);
+
+            listBoxWithUrls.ItemsSource = listOfUrls;
+
+            string[] htmlCodes = new string[listOfUrls.Count];
+
+            for (int i = 0; i < listOfUrls.Count; i++)
+            {
+                htmlCodes[i] = await Request.GetAsync(listOfUrls[i]);
+            }
+
+            int[] Counter = new int[listOfUrls.Count];
+
+            for (int i = 0; i < listOfUrls.Count; i++)
+            {
+                //string page = webBrowserFromGin.Document.Body.InnerText;
+
+                int count = 0, n = 0;
+
+                while ((n = htmlCodes[i].IndexOf(textFromSearchingTextBox, n)) != -1)
+                {
+                    n += textFromSearchingTextBox.Length;
+                    ++count;
+                }
+                Counter[i] = count;
+            }
+
+            listBoxWithCounts.ItemsSource = Counter;
+            //string htmlCode = await Request.GetAsync(listOfUrls[0]);
         }
     }
 }
---
+++ WpfHtmlReader/UrlValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfHtmlReader
+{
+    class UrlValidation
+    {
+        public static List<string> ValidationCheck(List<string> fileLines)
+        {
+            List<string> urlList = new List<string>();
+            Uri uriResult;
+            foreach (var line in fileLines)
+            {
+                bool result = Uri.TryCreate(line, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+                if (result == true)
+                {
+                    urlList.Add(line);
+                }
+            }
+            
+            return urlList;
+        }
+    }
+}
